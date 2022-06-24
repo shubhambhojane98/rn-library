b191fb00d88c96b4c8b484d1e7fd1a9bcffe14d8
Shubham B <shubham.bhojane@adapty.com>
2022-06-24 22:12:02 +0530

    Created TextInput Component

--- src/assets/svgs/index.tsx
+++ src/assets/svgs/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import {SvgProps} from 'react-native-svg';
 import GiftIcon from '../../assets/svgs/GiftIcon.svg';
+import HidePassword from '../../assets/svgs/HidePassword.svg';
+import ShowPassword from '../../assets/svgs/ShowPassword.svg';
 //=====Add icon name (your preference) over here and in Switch case
-export type IconNames = 'gifticon';
+export type IconNames = 'gifticon' | 'hidepassword' | 'showpassword';
 
 type Props = SvgProps & {name: IconNames};
 const IconSVG: React.FC<Props> = props => {
   switch (props.name) {
     case 'gifticon':
       return <GiftIcon {...props} />;
+    case 'hidepassword':
+      return <HidePassword {...props} />;
+    case 'showpassword':
+      return <ShowPassword {...props} />;
     default:
       return null;
   }
