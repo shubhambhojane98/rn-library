29dbe7f9b06698c2e8c2546a1eeb6abad746c1b5
Shubham B <shubham.bhojane@adapty.com>
2022-06-25 21:45:31 +0530

    Added  RadioButton

---
+++ src/Demo/ARadioButton-Demo.tsx
@@ -0,0 +1,28 @@
+import {SafeAreaView, ScrollView} from 'react-native';
+import React, {useState} from 'react';
+import ARadioButtonGroup from '../components/ARadioButtonGroup/ARadioButtonGroup';
+
+const ARadioButtonDemo = () => {
+  const RadioData = [
+    {id: 1, value: 'First'},
+    {id: 2, value: 'Second'},
+    {id: 3, value: 'Third'},
+    {id: 4, value: 'Four'},
+    {id: 5, value: 'Five'},
+  ];
+
+  const [radio, setRadio] = useState('');
+  console.log(radio);
+  return (
+    <SafeAreaView style={{marginHorizontal: 20}}>
+      <ScrollView>
+        <ARadioButtonGroup
+          onSelectValue={value => setRadio(value)}
+          data={RadioData}
+        />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default ARadioButtonDemo;
