4c6df8c33afe24256a88544354b0794d2036345c
Amit S <amitkumar.shetye@valtech.com>
2022-06-27 18:58:19 +0530

    exported updated components in index file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import {consoleLog, consoleWarn, consoleInfo} from './utils/Log';
+import {CheckboxStatus} from './components/ACheckbox/ACheckboxEnum';
 import {Color} from './theme';
 export {default as AButton} from './components/AButton/AButton';
 export {default as ATypography} from './components/ATypography/ATypography';
@@ -9,5 +10,10 @@ export {default as ARadioButton} from './components/ARadioButton/ARadioButton';
 export {default as ARadioButtonGroup} from './components/ARadioButtonGroup/ARadioButtonGroup';
 export {default as ATextInput} from './components/ATextInput/ATextInput';
 export {default as ASwitch} from './components/ASwitch/ASwitch';
+export {default as AModal} from './components/AModal/AModal';
+export {default as ASwitchSelector} from './components/ASwitchSelector/ASwitchSelector';
+export {default as AStarRating} from './components/AStarRating/AStarRating';
+export {default as AChecbox} from './components/ACheckbox/ACheckbox';
 export {consoleLog, consoleWarn, consoleInfo};
 export {Color};
+export {CheckboxStatus};
