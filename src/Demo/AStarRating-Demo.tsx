e3c92a120741862d57b2d879fb1387623031b743
Shubham B <shubham.bhojane@adapty.com>
2022-06-27 16:45:13 +0530

    resolved issue

--- src/Demo/AStarRating-Demo.tsx
+++ src/Demo/AStarRating-Demo.tsx
@@ -2,17 +2,22 @@ import React, {useState} from 'react';
 import {SafeAreaView, ScrollView} from 'react-native';
 import AStarRating from '../components/AStarRating/AStarRating';
 import ATypography from '../components/ATypography/ATypography';
+import {TypographyVariant} from '../components/ATypography/ATypographyEnum';
 
 const AStarRatingDemo = () => {
   const [starRating, setStarRating] = useState(0);
   return (
     <SafeAreaView style={{marginHorizontal: 20}}>
       <ScrollView>
-        <ATypography variant={'primaryBold'} style={{marginVertical: 20}}>
+        <ATypography
+          variant={TypographyVariant.PRIMARY_BOLD}
+          style={{marginVertical: 20}}>
           StarRating with default prop Example
         </ATypography>
         <AStarRating onSelectValue={value => setStarRating(value)} />
-        <ATypography variant={'primaryBold'} style={{marginVertical: 20}}>
+        <ATypography
+          variant={TypographyVariant.PRIMARY_BOLD}
+          style={{marginVertical: 20}}>
           {`StarRating Example with  maxStar && defaultRating && height && width`}
         </ATypography>
         <AStarRating
