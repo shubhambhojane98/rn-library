9703b36f926d3f719d439a75215d470d47c010fb
Shubham B <shubham.bhojane@adapty.com>
2022-06-28 13:14:34 +0530

    Added ProgressBar Component  & snapshot

---
+++ src/Demo/AProgressBar-Demo.tsx
@@ -0,0 +1,37 @@
+import {SafeAreaView} from 'react-native';
+import React from 'react';
+import AProgressBar from '../components/AProgressBar/AProgressBar';
+import ATypography from '../components/ATypography/ATypography';
+import {TypographyVariant} from '../components/ATypography/ATypographyEnum';
+import {Color} from '../theme';
+
+const AProgressBarDemo = () => {
+  return (
+    <SafeAreaView style={{marginHorizontal: 20, marginVertical: 20}}>
+      <ATypography
+        variant={TypographyVariant.PRIMARY_BOLD}
+        style={{marginVertical: 20}}>
+        TextInput with default prop
+      </ATypography>
+      <AProgressBar percentage={'70%'} />
+      <ATypography
+        variant={TypographyVariant.PRIMARY_BOLD}
+        style={{marginVertical: 20}}>
+        {`TextInput with  completedColor & backgroundColor prop`}
+      </ATypography>
+      <AProgressBar
+        completedColor={Color.blue}
+        backgroundColor={Color.lightgrey}
+        percentage={'70%'}
+      />
+      <ATypography
+        variant={TypographyVariant.PRIMARY_BOLD}
+        style={{marginVertical: 20}}>
+        {`TextInput with  borderRadius & height prop`}
+      </ATypography>
+      <AProgressBar height={15} borderRadius={8} percentage={'70%'} />
+    </SafeAreaView>
+  );
+};
+
+export default AProgressBarDemo;
