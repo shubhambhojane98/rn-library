57165e5d892dec10ae96864832f3b0ab06ad436c
Amit S <amitkumar.shetye@valtech.com>
2022-07-01 16:17:05 +0530

    Exported Animated Preloader in index file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import {consoleLog, consoleWarn, consoleInfo} from './utils/Log';
 import {CheckboxStatus} from './components/ACheckbox/ACheckboxEnum';
 import {Color} from './theme';
-import {TypographyVariant,TextAlignment} from './components/ATypography/ATypographyEnum';
+import {
+  TypographyVariant,
+  TextAlignment,
+} from './components/ATypography/ATypographyEnum';
 export {default as AButton} from './components/AButton/AButton';
 export {default as ATypography} from './components/ATypography/ATypography';
 export {default as AAvatar} from './components/AAvatar/AAvatar';
@@ -19,7 +22,8 @@ export {default as ACheckbox} from './components/ACheckbox/ACheckbox';
 export {default as AToast} from './components/AToast/AToast';
 export {default as AAlert} from './components/AAlert/AAlert';
 export {default as ADropDown} from './components/ADropDown/ADropDown';
+export {default as AAnimatedPreloader} from './components/AAnimatedPreloader/AAnimatedPreloader';
 export {consoleLog, consoleWarn, consoleInfo};
 export {Color};
 export {CheckboxStatus};
-export {TextAlignment,TypographyVariant}
+export {TextAlignment, TypographyVariant};
