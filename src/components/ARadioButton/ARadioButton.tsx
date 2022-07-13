83932a240b4842e86219b9fe30ea6c8286105ad1
Alap A <alap.anerao@valtech.com>
2022-07-13 13:45:25 +0530

    RadioButton : Removed unused import

--- src/components/ARadioButton/ARadioButton.tsx
+++ src/components/ARadioButton/ARadioButton.tsx
@@ -1,7 +1,6 @@
 import {View, StyleSheet} from 'react-native';
 import React, {FC} from 'react';
 import IconSVG from '../../assets/svgs';
-import {Color} from '../../theme';
 import {moderateScale} from 'react-native-size-matters';
 import {defaultScale} from '../../utils/Common';
 import ATypography from '../ATypography/ATypography';
