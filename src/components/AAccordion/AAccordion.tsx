a8b94def7228aa7b07bd3069d36622c705d73ee5
Alap A <alap.anerao@valtech.com>
2022-07-18 17:08:10 +0530

    Created AAccordion component with paper reference

--- src/components/AAccordion/AAccordion.tsx
+++ src/components/AAccordion/AAccordion.tsx
@@ -1,137 +1,212 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {
   View,
-  TouchableOpacity,
+  ViewStyle,
   StyleSheet,
-  LayoutAnimation,
-  Platform,
-  UIManager,
+  StyleProp,
+  TextStyle,
+  GestureResponderEvent,
+  TouchableOpacity,
 } from 'react-native';
+// import TouchableRipple from '../TouchableRipple/TouchableRipple';
+import {withTheme, useTheme} from '../../core/theming';
+import type {Theme} from '../../utils/types';
+import {ListAccordionGroupContext} from './AAccordionGroup';
+import IconSVG from '../../assets/svgs';
 import ATypography from '../ATypography/ATypography';
 import {TypographyVariant} from '../ATypography/ATypographyEnum';
-import {Color} from '../../theme';
-import IconSVG from '../../assets/svgs';
 
-interface AccourdionItem {
-  id: string | number;
+type Props = {
+  /**
+   * Title text for the list accordion.
+   */
   title: string;
-  component: any;
-}
+  /**
+   * Callback which returns a React element to display on the left side.
+   */
+  leftIconName?: any;
+  leftIconSize?: number;
+  leftIconStyle?: StyleProp<ViewStyle>;
+  /**
+   * Callback which returns a React element to display on the right side.
+   */
+  right?: (props: {isExpanded: boolean}) => any;
+  /**
+   * Whether the accordion is expanded
+   * If this prop is provided, the accordion will behave as a "controlled component".
+   * You'll need to update this prop when you want to toggle the component or on `onPress`.
+   */
+  expanded?: boolean;
+  /**
+   * Function to execute on press.
+   */
+  onPress?: () => void;
+  /**
+   * Function to execute on long press.
+   */
+  onLongPress?: (e: GestureResponderEvent) => void;
+  /**
+   * Content of the section.
+   */
+  children: any;
+  /**
+   * @optional
+   */
+  theme: Theme;
+  /**
+   * Style that is passed to the wrapping TouchableRipple element.
+   */
+  style?: StyleProp<ViewStyle>;
+  /**
+   * Style that is passed to Title element.
+   */
+  titleTextStyle?: StyleProp<TextStyle>;
 
-interface Prop {
-  data: AccourdionItem[];
-  marginTop?: number;
-  marginBottom?: number;
-  marginLeft?: number;
-  marginRight?: number;
-  separatorColor?: string;
-  fontSize?: number;
-  color?: string;
-}
+  /**
+   * Truncate Title text such that the total number of lines does not
+   * exceed this number.
+   */
+  titleNumberOfLines?: number;
+  /**
+   * Id is used for distinguishing specific accordion when using List.AccordionGroup. Property is required when using List.AccordionGroup and has no impact on behavior when using standalone List.Accordion.
+   */
+  id?: string | number;
+  /**
+   * TestID used for testing purposes
+   */
+  testID?: string;
+  /**
+   * Accessibility label for the TouchableRipple. This is read by the screen reader when the user taps the touchable.
+   */
+  accessibilityLabel?: string;
+};
 
-const defaultMargin = 0;
+const ListAccordion = ({
+  leftIconName,
+  leftIconSize,
+  leftIconStyle,
+  right,
+  title,
+  children,
+  titleTextStyle,
+  titleNumberOfLines = 1,
+  style,
+  id,
+  testID,
+  onPress,
+  onLongPress,
+  expanded: expandedProp,
+  accessibilityLabel,
+}: Props) => {
+  const {colors} = useTheme();
 
-const AAccordion: React.FC<Prop> = ({
-  data,
-  marginTop = defaultMargin,
-  marginBottom = defaultMargin,
-  marginLeft = defaultMargin,
-  marginRight = defaultMargin,
-  separatorColor = Color.greyOpac20,
-  fontSize = 16,
-  color = Color.black,
-}) => {
-  if (Platform.OS === 'android') {
-    UIManager.setLayoutAnimationEnabledExperimental(true);
-  }
-  const [expanded, setExpanded] = useState(false);
-  const [selectedItem, setSelectedItem] = useState({id: ''});
-  const [icon, SetIcon] = useState('downarrow');
+  const [expanded, setExpanded] = React.useState<boolean>(
+    expandedProp || false,
+  );
 
-  const stylesWithProps = styles({
-    marginTop,
-    marginRight,
-    marginBottom,
-    marginLeft,
-    separatorColor,
-  });
+  const handlePressAction = () => {
+    onPress?.();
 
-  const toggleExpand = (item: any) => {
-    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
-    setExpanded(true);
-    setSelectedItem(item);
+    if (expandedProp === undefined) {
+      // Only update state of the `expanded` prop was not passed
+      // If it was passed, the component will act as a controlled component
+      // eslint-disable-next-line @typescript-eslint/no-shadow
+      setExpanded(expanded => !expanded);
+    }
   };
 
+  const expandedInternal = expandedProp !== undefined ? expandedProp : expanded;
+
+  const groupContext = React.useContext(ListAccordionGroupContext);
+  if (groupContext !== null && !id) {
+    throw new Error(
+      'List.Accordion is used inside a List.AccordionGroup without specifying an id prop.',
+    );
+  }
+  const isExpanded = groupContext
+    ? groupContext.expandedId === id
+    : expandedInternal;
+  const handlePress =
+    groupContext && id !== undefined
+      ? () => groupContext.onAccordionPress(id)
+      : handlePressAction;
   return (
     <View>
-      {data.map(item => {
-        return (
-          <View key={item.id}>
-            <TouchableOpacity
-              style={stylesWithProps.row}
-              onPress={() => toggleExpand(item)}
-              activeOpacity={1}>
+      <View style={{backgroundColor: colors.background}}>
+        <TouchableOpacity
+          activeOpacity={1}
+          style={[styles.container, style]}
+          onPress={handlePress}
+          onLongPress={onLongPress}
+          accessibilityRole="button"
+          accessibilityState={{expanded: isExpanded}}
+          accessibilityLabel={accessibilityLabel}
+          testID={testID}
+          delayPressIn={0}>
+          <View style={styles.row} pointerEvents="none">
+            {leftIconName ? (
+              <View style={[styles.left, leftIconStyle]}>
+                <IconSVG
+                  name={leftIconName}
+                  height={leftIconSize || 24}
+                  width={leftIconSize || 24}
+                />
+              </View>
+            ) : null}
+            <View style={[styles.content]}>
               <ATypography
-                children={item.title}
-                fontSize={fontSize}
-                color={color}
-                variant={TypographyVariant.PRIMARY_BOLD}
-                style={stylesWithProps.titleStyle}
-              />
-              <IconSVG
-                name={selectedItem.id === item.id ? 'uparrow' : 'downarrow'}
-                height={20}
-                width={20}
-              />
-            </TouchableOpacity>
-            <View style={stylesWithProps.separator} />
-            {selectedItem.id === item.id && (
-              <View style={stylesWithProps.child}>{item.component}</View>
-            )}
+                numberOfLines={titleNumberOfLines}
+                style={[titleTextStyle]}
+                variant={TypographyVariant.PRIMARY_BOLD}>
+                {title}
+              </ATypography>
+            </View>
+            <View>
+              {right ? (
+                right({
+                  isExpanded: isExpanded,
+                })
+              ) : (
+                <IconSVG
+                  name={isExpanded ? 'uparrow' : 'downarrow'}
+                  height={24}
+                  width={24}
+                />
+              )}
+            </View>
           </View>
-        );
-      })}
+        </TouchableOpacity>
+      </View>
+      {isExpanded ? <View>{children}</View> : null}
     </View>
   );
 };
 
-export default AAccordion;
+ListAccordion.displayName = 'List.Accordion';
 
-const styles = (props: {
-  marginTop?: number;
-  marginRight?: number;
-  marginBottom?: number;
-  marginLeft?: number;
-  separatorColor?: string;
-}) =>
-  StyleSheet.create({
-    title: {
-      fontSize: 12,
-      fontFamily: 'DMSans-Medium',
-      color: Color.grey,
-      width: '80%',
-    },
-    row: {
-      flex: 1,
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      minHeight: 26,
-      paddingVertical: 12,
-      alignItems: 'center',
-      backgroundColor: 'transparent',
-      marginTop: props.marginTop,
-      marginRight: props.marginRight,
-      marginBottom: props.marginBottom,
-      marginLeft: props.marginLeft,
-    },
-    titleStyle: {flex: 0.95},
-    separator: {height: 1, backgroundColor: props.separatorColor},
-    child: {
-      backgroundColor: 'transparent',
-      padding: 0,
-      marginTop: props.marginTop,
-      marginRight: props.marginRight,
-      marginBottom: props.marginBottom,
-      marginLeft: props.marginLeft,
-    },
-  });
+const styles = StyleSheet.create({
+  container: {
+    height: 44,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  item: {
+    margin: 0,
+  },
+  child: {
+    paddingLeft: 64,
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  left: {
+    marginHorizontal: 10,
+  },
+});
+
+export default withTheme(ListAccordion);
