81bafbd4097094f456072440fa9a6363f53fe5f5
Prashant Kashid <prashant.kashidadapty@gmail.com>
2022-07-19 11:06:44 +0530

    Added Custom Radio Image and svg color prop

--- src/Demo/ARadioButton-Demo.tsx
+++ src/Demo/ARadioButton-Demo.tsx
@@ -1,4 +1,4 @@
-import {SafeAreaView, ScrollView} from 'react-native';
+import {Image, SafeAreaView, ScrollView} from 'react-native';
 import React, {useState} from 'react';
 import ARadioButtonGroup from '../components/ARadioButtonGroup/ARadioButtonGroup';
 import ATypography from '../components/ATypography/ATypography';
@@ -21,6 +21,27 @@ const ARadioButtonDemo = () => {
         <ARadioButtonGroup
           onSelectValue={(value: any) => setRadio(value)}
           data={RadioData}
+        />
+        <ATypography children="RadioButton with custom Images" />
+        <ARadioButtonGroup
+          onSelectValue={(value: any) => setRadio(value)}
+          data={RadioData}
+          selectedRadio={() => (
+            <Image
+              source={{
+                uri: 'https://static.thenounproject.com/png/195060-200.png',
+              }}
+              style={{height: 20, width: 20}}
+            />
+          )}
+          unselectedRadio={() => (
+            <Image
+              source={{
+                uri: 'https://icon-library.com/images/radio-button-icon/radio-button-icon-18.jpg',
+              }}
+              style={{height: 20, width: 20}}
+            />
+          )}
         />
       </ScrollView>
     </SafeAreaView>
