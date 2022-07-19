cde924e003a61ec42293d8e3123ddee180372347
Prashant Kashid <prashant.kashidadapty@gmail.com>
2022-07-19 15:02:42 +0530

    Updated test case for Typography

--- __tests__/__components__/ATypography-test.tsx
+++ __tests__/__components__/ATypography-test.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import ATypography from '../../src/components/ATypography/ATypography';
 import {TypographyVariant} from '../../src/components/ATypography/ATypographyEnum';
-import {Color} from '../../src/theme';
+import LightTheme from '../../src/theme/LightTheme';
 
 it('renders correctly with  props', () => {
   const tree = renderer
@@ -11,7 +11,7 @@ it('renders correctly with  props', () => {
       <ATypography
         children={'Text'}
         variant={TypographyVariant.PRIMARY_SEMI_BOLD}
-        color={Color.black}
+        color={LightTheme.colors.black}
         fontSize={16}
       />,
     )
